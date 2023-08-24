766e1489c4c56bf8ca1a4669a02a767fabe1c6b5
PrinceZugzwang <98677116+PrinceZugzwang@users.noreply.github.com>
2023-08-24 00:10:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import os
-from tpot import TPOTClassifier, TPOTRegressor  # Import TPOTRegressor
+from tpot import TPOTRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, mean_squared_error  # Import mean_squared_error
+from sklearn.metrics import mean_squared_error
 import joblib
 
 # Streamlit Sidebar
@@ -36,11 +36,6 @@ if choice == "ML":
 
         if y.dtypes == 'object':
             st.warning("Selected target column is categorical. Classification task will be performed.")
-            tpot = TPOTClassifier(generations=5,
-                                  population_size=20,
-                                  verbosity=2,
-                                  random_state=1,
-                                  config_dict='TPOT sparse')
         else:
             st.warning("Selected target column is numerical. Regression task will be performed.")
             tpot = TPOTRegressor(generations=5,
@@ -49,22 +44,22 @@ if choice == "ML":
                                  random_state=1,
                                  config_dict='TPOT sparse')
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-        tpot.fit(X_train, y_train)
+            if y.dtypes == 'datetime64[ns]':
+                y = (y - y.min()).dt.days  # Convert dates to days since the minimum date
+            elif y.dtypes == 'timedelta64[ns]':
+                y = y.dt.days  # Convert timedeltas to days
 
-        if y.dtypes == 'object':
-            y_pred = tpot.predict(X_test)
-            accuracy = accuracy_score(y_test, y_pred)
-            st.info(f"Accuracy: {accuracy:.2f}")
-        else:
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+            tpot.fit(X_train, y_train)
+
             y_pred = tpot.predict(X_test)
             mse = mean_squared_error(y_test, y_pred)
             st.info(f"Mean Squared Error: {mse:.2f}")
 
-        leaderboard = tpot.evaluated_individuals_
-        st.info("Model Leaderboard:")
-        st.dataframe(leaderboard)
+            leaderboard = tpot.evaluated_individuals_
+            st.info("Model Leaderboard:")
+            st.dataframe(leaderboard)
 
-        joblib.dump(tpot.fitted_pipeline_, 'best_model.pkl')
-        with open('best_model.pkl', 'rb') as f:
-            st.download_button("Download the Model", f, "best_model.pkl")
+            joblib.dump(tpot.fitted_pipeline_, 'best_model.pkl')
+            with open('best_model.pkl', 'rb') as f:
+                st.download_button("Download the Model", f, "best_model.pkl")
